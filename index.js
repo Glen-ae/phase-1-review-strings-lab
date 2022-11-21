3ce8a3475499ba54da8cf753ae91ab3ec51f0fb5
Glen <glen.kimani@student.moringaschool.com>
2022-11-21 17:44:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code in this file!
+ const currentUser = 'Grace Hopper';// Write your code in this file!
+ const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
+ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+ const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+ 'Edsger Dijkstra'.charAt(0);
+ 'Edsger Dijkstra'.length;
+ 'Edsger Dijkstra'[15];
+ 'Edsger Dijkstra'[14];
+ 'Edsger Dijkstra'.slice();
+ 'Edsger Dijkstra'.slice(7);
+ 'Edsger Dijkstra'.slice(0, 3);
+ 
+ 
+
+ 
